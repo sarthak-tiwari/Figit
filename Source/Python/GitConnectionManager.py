3c2b9e8911f6d6b815f2b4304a3234d6b469e067
sarthak-tiwari <43050725+sarthak-tiwari@users.noreply.github.com>
2018-09-23 16:32:48 -0700

    US06-TASK51: Added comments to all the current classes

--- Source/Python/GitConnectionManager.py
+++ Source/Python/GitConnectionManager.py
@@ -1,15 +1,37 @@
+#Class to manage all the connections between the application and GitHub API
+
+#import statements
 from github import Github
 
+#class definition
 class GitConnectionManager:
 
+    #constants to store username and password of the github profile which is used to_
+    #_access the api. Will be replaced with constant variables in the future
+    _USERNAME = "sarthak-tiwari"
+    _PASSWORD = "sarthak1!"
+
+
+
+    #holds the current active connection to the GitHub API
     _gitHubConnection = None
+
+
+
+    #function to provide access to gitHubConnection to other classes. This ensures that_
+    #_only a single connection exists at a given time. Also initializes the connection_
+    #_on the first run of the function.
 
     def getConnection(self):
         if(not(self._gitHubConnection)):
-            self._gitHubConnection = Github("sarthak-tiwari", "sarthak1!")
+            self._gitHubConnection = Github(self._USERNAME, self._PASSWORD)
 
         return self._gitHubConnection
 
+
+
+    #Function to validate whether a repository is a valid accessible GitHub repository or not._
+    #_Returns true if it is a valid repository, returns false if it is not.
     def isValidRepository(self, repositoryURL):
         
         connection = self.getConnection()
--- Source/Python/test.py
+++ Source/Python/test.py
@@ -1,7 +1,18 @@
+#Dummy file to simulate and test under development python classes
+
+
+
+#import statements
 from github import Github
 from GitConnectionManager import GitConnectionManager
 
-#g = Github("sarthak-tiwari", "sarthak1!")
+
+
+#----------------------------------------------------------------------------------------
+#Sample code to iterate through all the repositories belonging to a user_
+#_and print all the collaborators in each repository
+
+#g = Github("username", "password")
 
 #for repo in g.get_user().get_repos():
 #    print(repo.name)
@@ -9,6 +20,13 @@ from GitConnectionManager import GitConnectionManager
 
 #    for user in repo.get_collaborators():
 #        print("  - " + user.login)
+
+
+
+#----------------------------------------------------------------------------------------
+#Sample code showing an example of how to validate a repository on gitHub_
+#_using the GitConnectionManager  class
+
 manager = GitConnectionManager()
 
 if(manager.isValidRepository("sarthak-tiwari/ScrumDevils-SER_515")):
