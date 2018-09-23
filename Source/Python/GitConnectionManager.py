738c54641e972e3c563a2d98eb17bcc59c69864a
sarthak-tiwari <43050725+sarthak-tiwari@users.noreply.github.com>
2018-09-23 16:16:35 -0700

    US06-TASK51: created getConnection and isValidRepo function in GitConnectionManager

--- Source/Python/GitConnectionManager.py
+++ Source/Python/GitConnectionManager.py
@@ -2,5 +2,21 @@ from github import Github
 
 class GitConnectionManager:
 
+    _gitHubConnection = None
+
+    def getConnection(self):
+        if(not(self._gitHubConnection)):
+            self._gitHubConnection = Github("sarthak-tiwari", "sarthak1!")
+
+        return self._gitHubConnection
+
     def isValidRepository(self, repositoryURL):
-        return Py_RETURN_TRUE
+        
+        connection = self.getConnection()
+
+        try:
+            connection.get_repo(repositoryURL)
+        except:
+            return False
+
+        return True
--- Source/Python/test.py
+++ Source/Python/test.py
@@ -1,15 +1,17 @@
 from github import Github
 from GitConnectionManager import GitConnectionManager
 
-g = Github("sarthak-tiwari", "sarthak1!")
+#g = Github("sarthak-tiwari", "sarthak1!")
 
-for repo in g.get_user().get_repos():
-    print(repo.name)
-    #print(dir(repo))
+#for repo in g.get_user().get_repos():
+#    print(repo.name)
+#    #print(dir(repo))
 
-    for user in repo.get_collaborators():
-        print("  - " + user.login)
-
+#    for user in repo.get_collaborators():
+#        print("  - " + user.login)
 manager = GitConnectionManager()
-if(manager.isValidRepository("hi")):
-    print("function_worked")
+
+if(manager.isValidRepository("sarthak-tiwari/ScrumDevils-SER_515")):
+    print("valid repo")
+else:
+    print("invalid repo")
