2ac00d551049669e0e07796b5f53ba77bb7b607c
sarthak-tiwari <43050725+sarthak-tiwari@users.noreply.github.com>
2018-09-23 19:01:10 -0700

    US06-TASK51: Added exception handling to GitConnectionManager and created class documentation

--- Source/Python/GitConnectionManager.py
+++ Source/Python/GitConnectionManager.py
@@ -2,6 +2,7 @@
 
 #import statements
 from github import Github
+from github import GithubException
 
 #class definition
 class GitConnectionManager:
@@ -23,22 +24,38 @@ class GitConnectionManager:
     #_on the first run of the function.
 
     def getConnection(self):
+
         if(not(self._gitHubConnection)):
             self._gitHubConnection = Github(self._USERNAME, self._PASSWORD)
+
+        try:
+            for str in self._gitHubConnection.get_user().get_repos():
+                tmp = str
+        except GithubException as err:
+            if(err.status == 401):
+                print("Could not establish connection to GitHub. BadCredentialsException !")
+                return None
+            raise
 
         return self._gitHubConnection
 
 
 
     #Function to validate whether a repository is a valid accessible GitHub repository or not._
-    #_Returns true if it is a valid repository, returns false if it is not.
+    #_Returns true if it is a valid repository, returns false if it is not a valid repository or_
+    #_a connection with GitHub could not be established
     def isValidRepository(self, repositoryURL):
         
         connection = self.getConnection()
 
+        if(not(connection)):
+            return False
+
         try:
             connection.get_repo(repositoryURL)
-        except:
-            return False
+        except GithubException as err:
+            if(err.status == 404):
+                return False
+            raise
 
         return True
