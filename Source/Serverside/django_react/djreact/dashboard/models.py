526031a9f65a6e284352039b2da94a8a5389dde4
shefaliAJ7 <sanand22@asu.edu>
2018-10-28 16:58:16 -0700

    US59_TASK73: Understanding REST API and structure it to use later
    Through this, we can understand how to call REST API and get user
    specific data.
    The main task to get List of Collaborators is yet to be done. But this
    commit will help to understand the working of REST API better and will
    be available to other team mates, so that they follow this current
    structure

--- Source/Serverside/django_react/djreact/dashboard/models.py
+++ Source/Serverside/django_react/djreact/dashboard/models.py
@@ -3,5 +3,6 @@ from django.db import models
 # Create your models here.
 class User_Repositories(models.Model):
     application_username = models.CharField(max_length=100)
-    github_repository = models.CharField(max_length=200)
+    repository_link = models.CharField(max_length=200)
+    repository_name = models.CharField(max_length=200)
     cache_status = models.SmallIntegerField()
