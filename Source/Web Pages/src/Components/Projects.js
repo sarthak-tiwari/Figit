33a94e021cb36cb12fb6afaf6209107d22334219
Palak Chugh <pchugh1@asu.edu>
2018-10-31 13:19:33 -0700

    CONTRIBUTOR COMPONENT IS UPDATED Minor changes in the design has been made.

--- Source/Web Pages/src/Components/Projects.js
+++ Source/Web Pages/src/Components/Projects.js
@@ -4,6 +4,15 @@ import Displaycard from './Displaycard'
 import Piechart from './Piechart';
 
 class Projects extends Component {
+
+
+    state = {
+        myprofile: [
+          { photo:'https://i.pinimg.com/236x/e2/72/e4/e272e4c7495706cf18f565e2c0d36078--baby-harry-potter-rainbow-baby.jpg'},
+          { emailid: ['yrdesai@asu.edu','bpanda1@asu.edu','pchugh1@asu.edu','sanand22@asu.edu','bgoel1@asu.edu','stiwar22@asu.edu']}
+        ]
+      }
+
   render() {
     return (
 <div className="Projects">
@@ -16,15 +25,18 @@ class Projects extends Component {
 							<hr></hr>														
                 
 <p>
-    <Displaycard photo = "https://i.pinimg.com/236x/e2/72/e4/e272e4c7495706cf18f565e2c0d36078--baby-harry-potter-rainbow-baby.jpg" emailid = "yrdesai@asu.edu"/>
+    <Displaycard photo = {this.state.myprofile[0].photo} emailid = {this.state.myprofile[1].emailid[0]}/>
   </p>
- <p><Displaycard photo = "https://i.pinimg.com/236x/e2/72/e4/e272e4c7495706cf18f565e2c0d36078--baby-harry-potter-rainbow-baby.jpg" emailid = "pchugh1@asu.edu"/></p>
-<p><Displaycard photo = "https://i.pinimg.com/236x/e2/72/e4/e272e4c7495706cf18f565e2c0d36078--baby-harry-potter-rainbow-baby.jpg" emailid = "bpanda1@asu.edu"/></p>                    
+ <p><Displaycard photo = {this.state.myprofile[0].photo} emailid = {this.state.myprofile[1].emailid[1]}/></p>
+<p><Displaycard photo = {this.state.myprofile[0].photo} emailid = {this.state.myprofile[1].emailid[2]}/></p>                    
 <p>
-    <Displaycard photo = "https://i.pinimg.com/236x/e2/72/e4/e272e4c7495706cf18f565e2c0d36078--baby-harry-potter-rainbow-baby.jpg" emailid = "yrdesai@asu.edu"/>
+    <Displaycard photo = {this.state.myprofile[0].photo} emailid = {this.state.myprofile[1].emailid[3]}/>
   </p>
   <p>
-    <Displaycard photo = "https://i.pinimg.com/236x/e2/72/e4/e272e4c7495706cf18f565e2c0d36078--baby-harry-potter-rainbow-baby.jpg" emailid = "yrdesai@asu.edu"/>
+    <Displaycard photo = {this.state.myprofile[0].photo} emailid = {this.state.myprofile[1].emailid[4]}/>
+  </p>
+  <p>
+    <Displaycard photo = {this.state.myprofile[0].photo} emailid = {this.state.myprofile[1].emailid[5]}/>
   </p>
 
              </div>
