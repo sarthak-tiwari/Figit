03d21e111d401f15606b224de43c0b6dfa764b5d
shefaliAJ7 <sanand22@asu.edu>
2018-11-02 17:28:02 -0700

    US63-TASK94: Implemented API for Authenticated User Login
    Takes email or username and password from the frontend.
    Validates if the username or email exists.
    Validates if the password matches with the one given in database.

--- Source/Serverside/django_react/djreact/user_account/urls.py
+++ Source/Serverside/django_react/djreact/user_account/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from .views import (
     signup,
-    UserCreateAPIView
+    UserCreateAPIView,
+    UserLoginAPIView
 )
 
 urlpatterns = [
     url(r'^signup/',signup),
     url(r'^register/$', UserCreateAPIView.as_view(), name='register'),
+    url(r'^login/$', UserLoginAPIView.as_view(), name='login'),
 ]
--- Source/Serverside/django_react/djreact/user_account/views.py
+++ Source/Serverside/django_react/djreact/user_account/views.py
@@ -6,15 +6,16 @@ from rest_framework.response import Response
 from .models import Application_Users
 
 from django.contrib.auth import get_user_model
-from .serializers import Application_Users_Serializer, UserCreateSerializer
+from .serializers import Application_Users_Serializer, UserCreateSerializer, UserLoginSerializer
 
+from rest_framework.views import APIView
 from rest_framework.generics import (
     CreateAPIView,
     DestroyAPIView,
     ListAPIView, 
     UpdateAPIView,
     RetrieveAPIView,
-    RetrieveUpdateAPIView
+    RetrieveUpdateAPIView,
 )
 from rest_framework.permissions import (
     AllowAny,
@@ -39,3 +40,15 @@ User = get_user_model()
 class UserCreateAPIView(CreateAPIView):
     serializer_class = UserCreateSerializer
     queryset = User.objects.all()
+
+class UserLoginAPIView(APIView):
+    permission_classes = [AllowAny]
+    serializer_class = UserLoginSerializer
+    
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        serializer = UserLoginSerializer(data=data)
+        if serializer.is_valid(raise_exception=True):
+            new_data = serializer.data
+            return Response(new_data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
