733b4c095f193f69fa31fbc57485de6e1e948685
Bijayalaxmi Panda <bpanda1@asu.edu>
2018-11-03 23:27:47 -0700

    This commit creates a table to store commit data along with commit message. We extract the user name and commit message given by the user. And this json object is send to front end for display.

--- Source/Serverside/django_react/djreact/dashboard/serializers.py
+++ Source/Serverside/django_react/djreact/dashboard/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework.serializers import ModelSerializer
-from .models import User_Repositories, Repository_Collaborators
+from .models import User_Repositories, Repository_Collaborators, Repository_Commit_Data
 
 class User_Repositories_Serializer(ModelSerializer):
     class Meta:
@@ -10,3 +10,9 @@ class  Repository_Collaborators_Serializer(ModelSerializer):
     class Meta:
         model = Repository_Collaborators
         fields = ('id','application_username','repository_link','repository_name','collaborator_username','collaborator_name')
+
+
+class  commitCountInfo_Serializer(ModelSerializer):
+    class Meta:
+        model = Repository_Commit_Data
+        fields = ('commiterName','commitMessage')
