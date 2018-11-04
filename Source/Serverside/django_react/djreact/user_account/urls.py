7b430cf59ffa4a1edca5d193b6e99a495451588c
Bijayalaxmi Panda <bpanda1@asu.edu>
2018-11-04 00:18:20 -0700

    This commit is to add logout functionality in django. The api provided by django will automatically take care care of session destruction.

--- Source/Serverside/django_react/djreact/user_account/urls.py
+++ Source/Serverside/django_react/djreact/user_account/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.contrib.auth.views import LogoutView
 from .views import (
     signup,
     UserCreateAPIView,
@@ -9,4 +10,5 @@ urlpatterns = [
     url(r'^signup/',signup),
     url(r'^register/$', UserCreateAPIView.as_view(), name='register'),
     url(r'^login/$', UserLoginAPIView.as_view(), name='login'),
+    url(r'^logout/$', LogoutView.as_view(), name='logout'),
 ]
