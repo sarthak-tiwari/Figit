d98575bc0e242511c7d2c89cd18f80974c6c72d5
Yuti Desai <yrdesai@asu.edu>
2018-11-04 13:00:29 -0700

    US59-T83: FINAL VERSION OF PROFILE UI COMPONENT
    profile UI react component with dummy json data is added.

--- Source/Web Pages/src/App.js
+++ Source/Web Pages/src/App.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-import Profile from './Profile/Profile';
+import Flashcard from './Profile/Flashcard';
+import Flashcard2 from './Profile/Flashcard2';
+import Flashcard3 from './Profile/Flashcard3';
+import FlashcardData from './Profile/FlashcardData';
+import Profilejason from './Profile/Profilejason';
 
 class App extends Component {
-
-  state = {
-    myrofile: [
-      { photo:'https://i.pinimg.com/236x/e2/72/e4/e272e4c7495706cf18f565e2c0d36078--baby-harry-potter-rainbow-baby.jpg'},
-      { emailid:'yrdesai@asu.edu'}
-    ]
-  }
 
   render() {
     return(
       <div className ="App">
-      <Profile photo={this.state.myrofile[0].photo} emailid={this.state.myrofile[1].emailid}/>
-      </div>
+          <Profilejason />
+        </div>
     );
   }
 }
