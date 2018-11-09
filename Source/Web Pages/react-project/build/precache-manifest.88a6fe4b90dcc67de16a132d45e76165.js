97ea5939cb3e53076b6fe69a767e31095cb01612
sarthak-tiwari <43050725+sarthak-tiwari@users.noreply.github.com>
2018-11-09 00:08:22 -0700

    US95-TASK96: Created deployable sample files which will be depoyed at AWS to test

---
+++ Source/Web Pages/react-project/build/precache-manifest.88a6fe4b90dcc67de16a132d45e76165.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9d74abe76ea3eef426a5",
+    "url": "/static/js/main.9d74abe7.chunk.js"
+  },
+  {
+    "revision": "33b35ee87a7d6ad70a70",
+    "url": "/static/js/1.33b35ee8.chunk.js"
+  },
+  {
+    "revision": "9d74abe76ea3eef426a5",
+    "url": "/static/css/main.6635b6a6.chunk.css"
+  },
+  {
+    "revision": "3c16ca4966b34605456fa8b114ed97fb",
+    "url": "/index.html"
+  }
+];
