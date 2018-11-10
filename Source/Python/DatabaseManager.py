a51df5c7851c9e76d61200d9a1a78eb8c47d0f50
sarthak-tiwari <43050725+sarthak-tiwari@users.noreply.github.com>
2018-11-10 13:22:26 -0700

    US99-TASK101: Updated database manager class to handle pull request data

--- Source/Python/DatabaseManager.py
+++ Source/Python/DatabaseManager.py
@@ -54,11 +54,27 @@ class DatabaseManager:
 
         conn = DatabaseManager.getConnection()
 
-        query = 'INSERT INTO git_commit_data (github_repository, committer_name, commit_date, commit_message, \
-        number_of_additions, number_of_deletions, number_of_files_modified, link_to_github) VALUES (\'' + repositoryName + '\', ?, ?, ?, ?, ?, ?, ?)'
+        query = 'INSERT INTO git_pull_request_data (github_repository, request_id, requester_login, request_date, \
+                request_title, request_body, request_url) VALUES (?, ?, ?, ?, ?, ?, ?)'
 
-        for commit in commitData:
-            conn.execute(query, commit)
+        for request in pullRequestData:
+            conn.execute(query, request)
+
+        conn.commit()
+
+
+
+    #method to insert pull review data into the corrosponding table
+    @staticmethod
+    def insertPullRequestDataValues(pullReviewData):
+
+        conn = DatabaseManager.getConnection()
+
+        query = 'INSERT INTO git_pull_review_data (github_repository, request_id, reviewer_login, review_date, review_comment, \
+                review_url) VALUES (?, ?, ?, ?, ?, ?)'
+
+        for review in pullReviewData:
+            conn.execute(query, review)
 
         conn.commit()
     
