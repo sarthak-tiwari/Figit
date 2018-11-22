0bf716781260ca1972d75448354c40105f2aaa84
bpanda1 <bpanda1@asu.edu>
2018-11-22 16:05:29 -0700

    us105-task117: Opening Help page on a click

--- Source/figitfront/src/App.js
+++ Source/figitfront/src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
 import Help from './Help/Help';
+import PullRequest from './pullReq/pullReq';
 
 class App extends Component {
+  
+  showAlert() {
+    ReactDOM.render(<Help/>, document.getElementById('root'));
+  }
+  
   render() {
     return (
-      <div className="App">
-        <Help></Help>
-      </div>
+      <div>
+        <button onClick={this.showAlert}>Help Button</button>; 
+    </div>
     );
   }
 }
