9803a4139a03d63a475f9a801c710cc768c3a898
sarthak-tiwari <43050725+sarthak-tiwari@users.noreply.github.com>
2018-11-23 16:06:43 -0700

    US105-TASK117: Added CommitList and PullRequest list template

--- Source/figitfront/src/App.js
+++ Source/figitfront/src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Help from './Help/Help';
+import CommitList from './CommitList/CommitList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Help></Help>
+        <CommitList></CommitList>
       </div>
     );
   }
