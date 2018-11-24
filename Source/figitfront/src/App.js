b132b0459cb371526d1d4e1cef0ab04f489050e9
bpanda1 <bpanda1@asu.edu>
2018-11-24 02:02:48 -0700

    us105-task117: Basic Line graph for commit Data in App.js

--- Source/figitfront/src/App.js
+++ Source/figitfront/src/App.js
@@ -5,17 +5,73 @@ import ReactDOM from 'react-dom';
 import Help from './Help/Help';
 import PullRequest from './pullReq/pullReq';
 
+
 class App extends Component {
-  
-  showAlert() {
-    ReactDOM.render(<Help/>, document.getElementById('root'));
+  constructor(){
+    super();
+    this.state = {
+      chartData:{}
+    }
+    var test = new Object();
+  }
+
+  componentWillMount(){
+    this.getChartData();
+  }
+
+  getChartData() {
+    var test = [
+      [['x1', "bpanda1"], ['x2', 10]],
+      [['x1', "bpanda2"], ['x2', 7]],
+      [['x1', "bpanda3"], ['x2', 17]],
+      [['x1', "bpanda4"], ['x2', 4]]
+    ];
+    // Ajax calls here
+    var xValues = [];
+    var yValues = [];
+    for(var i=0; i<test.length; i++) {
+        xValues.push(test[i][0][1]);
+        yValues.push(test[i][1][1]);
+    }
+    var yAxisName = "Count";
+    
+    var backgroundColor = [
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+              'rgba(153, 102, 255, 0.6)',
+              'rgba(255, 159, 64, 0.6)',
+              'rgba(255, 99, 132, 0.6)'
+            ]
+    this.setState({
+      
+      chartData:{
+        labels: xValues,
+        datasets:[
+          {
+            label:yAxisName,
+            data: yValues,
+            backgroundColor: 'blue',
+            fill:false,
+            lineTension: 0
+          }
+        ]
+      }
+    });
+  }
+
+
+  myFunction() {
+    alert("I am an alert box!");
   }
   
   render() {
     return (
       <div>
-        <button onClick={this.showAlert}>Help Button</button>; 
-    </div>
+        <PullRequest chartData={this.state.chartData} location="Commit Graph" legendPosition="bottom"/>  
+      </div>
+
     );
   }
 }
