6555fed6babbcaa956ebf18ce2e52e5ddb49a4a2
bpanda1 <bpanda1@asu.edu>
2018-11-24 02:01:57 -0700

    us105-task117: Basic Line graph for commit Data

--- Source/figitfront/src/pullReq/pullReq.js
+++ Source/figitfront/src/pullReq/pullReq.js
@@ -1,15 +1,44 @@
 import React from 'react';
-import './pullReq.css';
+//import './pullReq.css';
 import Pin from './logo.png';
+import {Bar, Line, Pie} from 'react-chartjs-2';
 
 class PullRequest extends React.Component {
-    render() {
-    return(
-		<div> 
-			<p>Hello World!!</p>
-		</div>
-		
-         );  
-    }
+	constructor(props){
+		super(props);
+		this.state = {
+		  chartData:props.chartData
+		}
+	  }
+	
+	  static defaultProps = {
+		displayTitle:true,
+		displayLegend: true,
+		legendPosition:'right',
+		location:'City',
+	  }
+
+	  render() {
+		  return (
+			<div className="chart">
+				
+				<Line
+					data={this.state.chartData}
+					options={{
+						title:{
+						display:this.props.displayTitle,
+						text:' '+this.props.location,
+						fontSize:25
+						},
+						legend:{
+						 display:this.props.displayLegend,
+						 position:this.props.legendPosition
+						},
+					}}
+        		/>
+			</div>  
+		  )
+	  }
 }
+
 export default PullRequest;
