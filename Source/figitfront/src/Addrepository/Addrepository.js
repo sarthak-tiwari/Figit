f29a79f2e6437c074d52b79e222ac0578878e47e
sarthak-tiwari <43050725+sarthak-tiwari@users.noreply.github.com>
2018-11-24 20:28:19 -0700

    us105-task117-fork: updated code with UI bug fix

--- Source/figitfront/src/Addrepository/Addrepository.js
+++ Source/figitfront/src/Addrepository/Addrepository.js
@@ -2,58 +2,57 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 import './Addrepository.css';
 import '../index.js';
+import Pin from './logo2.png'
 import $ from 'jquery';
 
 class Addrepository extends React.Component {
 
-        constructor(props) {
-            super(props);
+  constructor(props) {
+    super(props);
 
-            this.onSubmit = this.onSubmit.bind(this);
-            this.onAdd = this.onAdd.bind(this);
-        }
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onAdd = this.onAdd.bind(this);
+  }
 
+  onSubmit (event) {
+    event.preventDefault();
+    alert("submitted");
+    this.redirectToTarget();
+  }
 
-        onSubmit (event) {
-           event.preventDefault();
-           alert("submitted");
-           this.redirectToTarget();
-         }
-
-        redirectToTarget = () => {
-            this.props.history.push('/dashboard');
-        }
-        onAdd(){
-          var i = 0;
-          if (document.getElementById('link').value != "") {
-              i++;
-              $('#dynamic_field').append(
-                  '<tr id="row' + i + '">+\
-                <td><p readonly type="text" +\
-                id="repo' + i + '" +\
-                class="form-control name_list"> ' + $('#link').val() + '</p></td> +\
-                <td><button type="button" +\
-                name="remove" id="' + i + '" class="btn btn-danger btn_remove"><i class="fas fa-trash-alt"></i></button></td>+\
-                </tr>'
-              );
-              document.getElementById('link').value = "";
-              $("#submit").removeAttr('disabled');
-          } 
-        }
-
+ redirectToTarget = () => {
+     this.props.history.push('/dashboard');
+ }
+ 
+ onAdd(){
+   var i = 0;
+   if (document.getElementById('link').value != "") {
+       i++;
+       $('#dynamic_field').append(
+        '<tr id="row' + i + '">+\
+        <td><p readonly type="text" +\
+        id="link' + i + '" +\
+        class="form-control name_list intext"> ' + $('#link').val() + '</p></td> +\
+        <td><button type="button" +\
+        name="remove" id="' + i + '" class="btnadd btn-danger btn_remove"><i class="fas fa-trash-alt"></i></button></td>+\
+        </tr>'
+      );
+       document.getElementById('link').value = "";
+       $("#submit").removeAttr('disabled');
+   } 
+ }
 
 render(){
   return(
     <div className = "body">
       <div className="topnav">
-        <a className="active" href="C:\Users\desai\Desktop\bootland.html">
-          <img className="logo-img" src="C:/Users/desai/Desktop/logo.png" alt="Smiley face"/>
-        </a>
-        <div className="topnav-right" style={{marginTop: 25+'px'}}>
-          <a href="#search">Help</a>
-          <a href="#about">About</a>
+            <a className="active" href="C:/Users/desai/Desktop/bootland.html"><img src={Pin} alt="FigitLogo"/></a>
+              <div className="topnav-right">
+                <a href="C:/Users/desai/Desktop/help.html">Help</a>
+                <a href="C:/Users/desai/Desktop/about.html">About</a>
+              </div>
         </div>
-      </div>
+        <br/><br/><br/><br/><br/>
       <div className="header">
         <p>You need to add some repositories!</p>
       </div>
@@ -62,17 +61,17 @@ render(){
           <form name="add_name" id="add_name" onSubmit={(event)=>{this.onSubmit(event)}} action="#">
             <div className="table-responsive">
               <div className="d-flex justify-content-center">
-                <table className="table table-bordered" id="dynamic_field" style={{width: 650+'px'}}>
+                <table className="table" id="dynamic_field" style={{width: 650+'px'}}>
                   <tbody>
                     <tr>
-                      <td><input type="text" id="link"  placeholder="Paste your GitHub repository link here" className="form-control name_list" /></td>
-                      <td><button type="button" name="add" onClick={this.onAdd} id="add" className="btn btn-success"><i className="fas fa-plus"></i> Add More</button></td>
+                      <td><input type="text" id="link"  placeholder="Paste your GitHub repository link here" className="form-control intext name_list" /></td>
+                      <td><button type="button" name="add" onClick={this.onAdd} id="add" className="btnadd btn-success"><i className="fas fa-plus"></i> Add More</button></td>
                     </tr>
                   </tbody>
                 </table>
               </div>
               <div className="d-flex justify-content-center">
-                <button className="btn" disabled type="submit" id="submit"
+                <button className="btnadd" disabled type="submit" id="submit"
                 style={{width: 190+'px',height: 45+'px',padding: 1+'px',borderRadius: 4+'px', fontSize: 17+'px'}} >Go to Dashboard</button>
               </div>
             </div>
