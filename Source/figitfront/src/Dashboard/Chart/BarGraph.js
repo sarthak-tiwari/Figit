048cf6d075adb9d929d7261d50a3f156d53feceb
sarthak-tiwari <43050725+sarthak-tiwari@users.noreply.github.com>
2018-11-25 18:45:53 -0700

    us105-task117-fork: Updated bar graph to work with api

--- Source/figitfront/src/Dashboard/Chart/BarGraph.js
+++ Source/figitfront/src/Dashboard/Chart/BarGraph.js
@@ -5,49 +5,70 @@ import './BarGraph.css';
 class BarGraph extends React.Component {
 	
     constructor(){
+
         super();
+
         this.state = {
-          chartData:{}
+          chartData:{},
+          location:''
         }
+
         var test = new Object();
       }
     
       componentWillMount(){
-        this.getChartData();
-      }
-    
-      newClick(){
-        if(document.getElementById('commit_bar').checked) {
-            document.getElementById('commitselect_bar').style.display = "block";
-            document.getElementById('pullreqselect_bar').style.display = "none";
-         } 
-         else if (document.getElementById('pullrequest_bar').checked) {
-           document.getElementById('commitselect_bar').style.display = "none";
-           document.getElementById('pullreqselect_bar').style.display = "block";
-         }
+        this.fetchData("commitCount");
       }
 
-      getChartData() {
-        var test = [
-          [['x1', "Jan 2 2018"], ['x2', 10]],
-          [['x1', "Jan 8 2018"], ['x2', 13]],
-          [['x1', "Nov 13 2018"], ['x2', 0]],
-          [['x1', "Dec 13 2018"], ['x2', 15]]
-        ];
-
+      fetchData(param){
         
-        
-        // Ajax calls here
-        var xValues = [];
-        var yValues1 = [4,5,9,12];
-        var yValues = [];
-        for(var i=0; i<test.length; i++) {
-            xValues.push(test[i][0][1]);
-            yValues.push(test[i][1][1]);
+        var url = '';
+        var username = 'committer_name';
+        var detailname = 'commit_details';
+        var countname = 'commit_count';
+        var location = '';
+        if(param === "commitCount") {
+          url = 'http://localhost:8000/dashboard/bargraph/commit_count/ScrumDevils-SER_515/';
+          location = 'Commit contribution per week';
         }
-        var yAxisName = "Commit";
-        var yAxisName1 = "Pull";
-        var backgroundColor = [
+        else if(param === "pullReqCount") {
+            url = 'http://localhost:8000/dashboard/timeline/pull_request_count/ScrumDevils-SER_515/';
+            location = 'Pull Request contribution per week';
+            username = 'requester_name';
+            detailname = 'request_details';
+            countname = 'request_count';
+        }
+
+        fetch(url, {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'}
+	      })
+        .then(response => {return(response.json())})
+        .then(response => {this.getChartData(response, username, detailname, countname, location)})
+      }
+
+      getChartData(test, username, detailname, countname, loc) {
+        
+      var commiterName = [];
+      var xValuesDict = {};
+      var yValuesDict = {};
+      
+      // Prepare the Data as per the JSON value
+      for(var i=0; i<test.length; i++) {
+          var xValues = [];
+          var yValues = [];
+          commiterName.push(test[i][username]);
+          for (var j = 0; j<test[i][detailname].length; j++) {
+            xValues.push(test[i][detailname][j]['week_name']);
+            yValues.push(test[i][detailname][j][countname]);
+          }   
+          xValuesDict[i] = xValues;
+          yValuesDict[i] = yValues;
+      }
+  
+      var backgroundColor = [
                   'rgba(255, 99, 132, 0.6)',
                   'rgba(54, 162, 235, 0.6)',
                   'rgba(255, 206, 86, 0.6)',
@@ -55,50 +76,46 @@ class BarGraph extends React.Component {
                   'rgba(153, 102, 255, 0.6)',
                   'rgba(255, 159, 64, 0.6)',
                   'rgba(255, 99, 132, 0.6)'
-                ]
-        this.setState({
-          
+      ]
+      // Build the chartData as per contributer list
+      var dataSetValues = [];
+      var chartXValues = [];
+      for (var i = 0; i < commiterName.length; i++) {
+          var dict = {};
+          dict['label'] =commiterName[i];
+          dict['data'] = yValuesDict[i];
+          dict['default'] = 0;
+          dict['backgroundColor'] = backgroundColor[i];
+          dict['fill'] = false;
+          dict['lineTension'] = 0;
+          dict['stacked'] = true;
+          dict['fillGapsWithZero'] = true;
+          dataSetValues.push(dict);
+      }
+        
+      var chartDataArr = [];
+      for (var i = 0; i < commiterName.length; i++){
+          var chartDataValues = {};
+          chartDataValues['labels'] = commiterName[i];
+          chartDataArr.push(commiterName[i]);
+      }
+  
+      this.setState({
           chartData:{
             labels: xValues,
-            datasets:[
-              {
-                label:yAxisName,
-                data: yValues,
-                default: 0,
-                backgroundColor: 'pink',
-                fill:false,
-                lineTension:0,
-                stacked:true,
-                fillGapsWithZero: true,
-                barThickness:'flex'
-
-              },
-              {
-                label:yAxisName1,
-                data: yValues1,
-                default: 0,
-                backgroundColor: 'blue',
-                fill:false,
-                lineTension:0,
-                stacked:true,
-                fillGapsWithZero: true,
-                barThickness:'flex'
-              }
-            ]
-          }
-        });
-      }
+            datasets: dataSetValues
+          },
+          location:loc
+      });
+    }
 	
-	static defaultProps = {
-		displayTitle:true,
-		displayLegend: true,
-		legendPosition:'right',
-	    location:'City',
+	  static defaultProps = {
+		  displayTitle:true,
+		  displayLegend: true,
+		  legendPosition:'right'
     }
     
-
-
-	  render() {
+    render() {
 		  return (
 			<div class="bargraph">
 				<Bar
@@ -106,7 +123,7 @@ class BarGraph extends React.Component {
 					options={{
 						title:{
 						 display:this.props.displayTitle,
-						 text:' '+this.props.location,
+						 text:this.state.location,
 						 fontSize:25
 						},
 						legend:{
@@ -122,35 +139,18 @@ class BarGraph extends React.Component {
               }]
           }
 					}}
-        		/>
+        />
 
-                 <div class="select">
-                    <form id="dataselect_bar" onClick= {this.newClick}>
-                        <input type="radio" name="valuetype" value="commit" id="commit_bar" defaultChecked/> Commits
-                        <div class="pull_bar">
-                        <input type="radio" name="valuetype" value="pullrequest" id="pullrequest_bar"/> Pull Requests
-                        </div>
-                    </form>
-                    <br/>
-                    
-                    <form id="commitselect_bar" >
-                        <input type="radio" name="committype" value="count" defaultChecked/> Commits counts<br/>
-                        <input type="radio" name="committype" value="adds"/> No. of additions<br/>
-                        <input type="radio" name="committype" value="dels"/> No. of deletions<br/>
-                        <input type="radio" name="committype" value="filmod"/> Files modified<br/>
-                    </form>
-                    
-
-                    <form id="pullreqselect_bar" >
-                        <input type="radio" name="pullreqtype" value="count" defaultChecked/> Count<br/>
-                        <input type="radio" name="pullreqtype" value="raised"/> Raised<br/>
-                        <input type="radio" name="pullreqtype" value="reviewed"/> Reviewed<br/>
-                        <br/>
-                    </form>
-
-                </div>
-
-			</div>  
+        <div class="select">
+            <form id="dataselect_bar" onClick= {this.newClick}>
+              <input type="radio" name="valuetype" value="commit" onClick={(event)=>{this.fetchData('commitCount')}} id="commit_bar" defaultChecked/> Commit Counts
+              <div class="pull_bar">
+                 <input type="radio" name="valuetype" value="pullrequest" onClick={(event)=>{this.fetchData('pullReqCount')}} id="pullrequest_bar"/> Pull Requests
+              </div>
+            </form>
+            <br/>
+        </div>
+      </div>  
 		  )
 	  }
 }
